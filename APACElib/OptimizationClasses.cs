1e105f2985ceafa3731ce2b4c5b2bc16c62fc04e
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-23 21:00:53 -0400

    minor

--- APACElib/OptimizationClasses.cs
+++ APACElib/OptimizationClasses.cs
@@ -317,7 +317,7 @@ namespace APACElib
                 maxItrs: modelSets.OptmzSets.NOfItrs,
                 nLastItrsToAve: modelSets.OptmzSets.NOfLastItrsToAverage,
                 x0: x0,
-                ifParallel: false,
+                ifParallel: true,
                 modelProvidesDerivatives: true
                 );
 
