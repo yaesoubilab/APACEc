1384a202ab4d458c668a10e0edf514f6c45dac7b
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-07-22 20:35:36 -0400

    organized small gono classes

--- APACElib/Models/GonoModels.cs
+++ APACElib/Models/GonoModels.cs
@@ -10,25 +10,73 @@ using RandomVariateLib;
 namespace APACElib.Models
 {
     // used for total statis over all districts 
-    enum GonoSpecialStatIDs { PopSize = 0, Prev, FirstTx, SuccessAOrB, SuccessAOrBOrM, PercFirstTxAndResist }
+    //enum GonoSpecialStatIDs { PopSize = 0, Prev, FirstTx, SuccessAOrB, SuccessAOrBOrM, PercFirstTxAndResist }
+    //enum Features { Time = 0, PercResist, ChangeInPercResist }
+    enum GonoSpecialStatIDs { PopSize = 0, Prev, Tx1, Tx1Sym, Tx1Resist, SuccessAOrB, SuccessAOrBOrM, PercFirstTxAndResist }
     enum Features { Time = 0, PercResist, ChangeInPercResist }
+    enum Conditions { AOut = 0, BOut, ABOut, AOk, BOk, ABOk, BNeverUsed, M1NeverUsed, AOn, AOff, BOn, BOff, MOn, MOff };
+    enum Interventions {A1=2, B1, M1, B2_A, M2_A, M2_B_AB }  
+                        // A1:    A is used for 1st line treatment
+                        // B1:    B is used for 1st line treatment
+                        // M1:    M is used for 1st line treatment
+                        // B2_A:  retreating those infected with G_A with B after 1st line treatment failure
+                        // M2_A:  retreating those infected with G_A with M after 1st line treatment failure
+                        // M2_B_AB: retreating those infected with G_B or G_AB with M after 1st line treatment failure
+
+    public class GonoInterventionInfo
+    {
+        public List<int> InterventionsIDs { get; set; }
+
+        // TODO: needs to be tested
+        public void Reset(int nRegions)
+        {
+            InterventionsIDs = new List<int>(new int[Enum.GetValues(typeof(Interventions)).Length]);
+
+            foreach (Interventions intv in Enum.GetValues(typeof(Interventions)))
+            {
+                // 2 is added for the Default and Always Off interventions 
+                if (nRegions > 1)
+                    InterventionsIDs[(int)intv] = nRegions * (int)intv + 2;
+                else
+                    InterventionsIDs[(int)intv] = (int)intv + 2;
+            }
+        }
+    }
+
+    public class GonoConditionsInfo
+    {
+        public List<int> ConditionIDs { get; set; }
+
+        public void Reset(int nRegions)
+        {
+            ConditionIDs = new List<int>(new int[Enum.GetValues(typeof(Conditions)).Length]);
+
+            foreach (Conditions c in Enum.GetValues(typeof(Conditions)))
+            {
+                if (nRegions > 1)
+                    ConditionIDs[(int)c] = (nRegions + 1) * (int)c;
+                else
+                    ConditionIDs[(int)c] = (int)c;
+            }
+        }
+    }
 
     public class GonoSpecialStatInfo
     {
         public List<int> SpecialStatIDs { get; set; }
+
         public List<string> Prev { get; set; }
         public List<List<string>> PrevSym { get; set; } // Sym, Asym
         public List<List<string>> PrevResist { get; set; } // 0, A, B, AB
-
         public List<string> Treated { get; set; }
         public List<string> TreatedAndSym { get; set; } // Sym, Asym
         public List<List<string>> TreatedResist { get; set; } // 0, A, B, AB
-
         public List<string> TreatedA1B1B2 { get; set; } // A1, B1, B2
         public List<string> TreatedM1M2 { get; set; } // M1, M2
 
-        public List<int> SumTxResistFirstRegion { get; set; }
-        public List<int> RatioTxResistFirstRegion { get; set; }
+        public int SumTxFirstRegion { get; set; }
+        public List<int> SumTxResistFirstRegion { get; set; } // 0, A, B, AB
+        public List<int> RatioTxResistFirstRegion { get; set; } // 0, A, B, AB 
 
         public void Reset(int nRegions)
         {
@@ -40,10 +88,11 @@ namespace APACElib.Models
             TreatedAndSym = new List<string>();
             TreatedResist = new List<List<string>>();
 
+            SumTxFirstRegion = 0;
             SumTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
             RatioTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
 
-            for (int i = 0; i < nRegions; i++)
+            for (int i = 0; i < nRegions + 1; i++)
             {
                 Prev.Add("");
                 PrevSym.Add(new List<string>() { "", "" }); // Sym, Asym
@@ -74,6 +123,68 @@ namespace APACElib.Models
             PChangeInPercResistFirstRegion = new int[4]; // 4 for 0, A, B, AB
         }
     }
+
+    //public class GonoSpecialStatInfo
+    //{
+    //    public List<int> SpecialStatIDs { get; set; }
+    //    public List<string> Prev { get; set; }
+    //    public List<List<string>> PrevSym { get; set; } // Sym, Asym
+    //    public List<List<string>> PrevResist { get; set; } // 0, A, B, AB
+
+    //    public List<string> Treated { get; set; }
+    //    public List<string> TreatedAndSym { get; set; } // Sym, Asym
+    //    public List<List<string>> TreatedResist { get; set; } // 0, A, B, AB
+
+    //    public List<string> TreatedA1B1B2 { get; set; } // A1, B1, B2
+    //    public List<string> TreatedM1M2 { get; set; } // M1, M2
+
+    //    public List<int> SumTxResistFirstRegion { get; set; }
+    //    public List<int> RatioTxResistFirstRegion { get; set; }
+
+    //    public void Reset(int nRegions)
+    //    {
+    //        SpecialStatIDs = new List<int>(new int[Enum.GetValues(typeof(GonoSpecialStatIDs)).Length]);
+    //        Prev = new List<string>();
+    //        PrevSym = new List<List<string>>();
+    //        PrevResist = new List<List<string>>();
+    //        Treated = new List<string>();
+    //        TreatedAndSym = new List<string>();
+    //        TreatedResist = new List<List<string>>();
+
+    //        SumTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
+    //        RatioTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
+
+    //        for (int i = 0; i < nRegions; i++)
+    //        {
+    //            Prev.Add("");
+    //            PrevSym.Add(new List<string>() { "", "" }); // Sym, Asym
+    //            PrevResist.Add(new List<string> { "", "", "", "" }); // 0, A, B, AB
+    //            Treated.Add("");
+    //            TreatedAndSym.Add("");
+    //            TreatedResist.Add(new List<string> { "", "", "", "" }); // 0, A, B, AB
+    //        }
+
+    //        TreatedA1B1B2 = new List<string>() { "", "", "" };
+    //        TreatedM1M2 = new List<string>() { "", "" };
+    //    }
+    //}
+
+    //public class GonoFeatureInfo
+    //{
+    //    public List<int> FeatureIDs { get; set; }
+    //    public int[] PercResistFirstRegion { get; set; }
+    //    public int[] PChangeInPercResistFirstRegion { get; set; }
+    //    public int IfAEverOff { get; set; }
+    //    public int IfBEverOff { get; set; }
+    //    public int IfMEverOn { get; set; }
+
+    //    public void Reset(int nRegions)
+    //    {
+    //        FeatureIDs = new List<int>(new int[Enum.GetValues(typeof(Features)).Length]);
+    //        PercResistFirstRegion = new int[4]; // 4 for 0, A, B, AB
+    //        PChangeInPercResistFirstRegion = new int[4]; // 4 for 0, A, B, AB
+    //    }
+    //}
 
     public class MSMGonoModel : GonoModel
     {       
--- APACElib/Models/MasterGonoModel.cs
+++ APACElib/Models/MasterGonoModel.cs
@@ -6,94 +6,17 @@ using System.Threading.Tasks;
 using APACElib;
 using ComputationLib;
 using RandomVariateLib;
+using APACElib.Models;
 
 namespace APACElib
 {
     // used for total statis over all districts 
-    enum GonoSpecialStatIDs { PopSize = 0, Prev, Tx1, Tx1Sym, Tx1Resist, SuccessAOrB, SuccessAOrBOrM, PercFirstTxAndResist }
-    enum Features { Time = 0, PercResist, ChangeInPercResist}
-    enum Conditions { AOut=0, BOut, ABOut, AOk, BOk, ABOk, BNeverUsed, M1NeverUsed, AOn, AOff, BOn, BOff, MOn, MOff };
+    
+    
 
-    public class GonoSpecialStatInfo
-    {        
-        public List<int> SpecialStatIDs { get; set; }
 
-        public List<string> Prev { get; set; }
-        public List<List<string>> PrevSym { get; set; } // Sym, Asym
-        public List<List<string>> PrevResist { get; set; } // 0, A, B, AB
-        public List<string> Treated { get; set; }
-        public List<string> TreatedAndSym { get; set; } // Sym, Asym
-        public List<List<string>> TreatedResist { get; set; } // 0, A, B, AB
-        public List<string> TreatedA1B1B2 { get; set; } // A1, B1, B2
-        public List<string> TreatedM1M2 { get; set; } // M1, M2
 
-        public int SumTxFirstRegion { get; set; }
-        public List<int> SumTxResistFirstRegion { get; set; } // 0, A, B, AB
-        public List<int> RatioTxResistFirstRegion { get; set; } // 0, A, B, AB 
-
-        public void Reset(int nRegions)
-        {
-            SpecialStatIDs = new List<int>(new int[Enum.GetValues(typeof(GonoSpecialStatIDs)).Length]);
-            Prev = new List<string>();
-            PrevSym = new List<List<string>>();
-            PrevResist = new List<List<string>>();
-            Treated = new List<string>();
-            TreatedAndSym = new List<string>();
-            TreatedResist = new List<List<string>>();
-
-            SumTxFirstRegion = 0;
-            SumTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
-            RatioTxResistFirstRegion = new List<int>(new int[4]); // 4 for 0, A, B, AB
-
-            for (int i = 0; i < nRegions + 1; i++)
-            {
-                Prev.Add("");
-                PrevSym.Add(new List<string>() { "", "" }); // Sym, Asym
-                PrevResist.Add(new List<string> { "", "", "", "" }); // 0, A, B, AB
-                Treated.Add("");
-                TreatedAndSym.Add("");
-                TreatedResist.Add(new List<string> { "", "", "", "" }); // 0, A, B, AB
-            }
-
-            TreatedA1B1B2 = new List<string>() { "", "", "" };
-            TreatedM1M2 = new List<string>() { "", ""};
-        }
-    }
-
-    public class GonoFeatureInfo
-    {        
-        public List<int> FeatureIDs { get; set; }
-        public int[] PercResistFirstRegion { get; set; }
-        public int[] PChangeInPercResistFirstRegion { get; set; }
-        public int IfAEverOff { get; set; }
-        public int IfBEverOff { get; set; }
-        public int IfMEverOn { get; set; }
-
-        public void Reset(int nRegions)
-        {
-            FeatureIDs = new List<int>(new int[Enum.GetValues(typeof(Features)).Length]);
-            PercResistFirstRegion = new int[4]; // 4 for 0, A, B, AB
-            PChangeInPercResistFirstRegion = new int[4]; // 4 for 0, A, B, AB
-        }
-    }
-
-    public class GonoConditionsInfo
-    {
-        public List<int> ConditionIDs { get; set; }
-
-        public void Reset(int nRegions)
-        {
-            ConditionIDs = new List<int>(new int[Enum.GetValues(typeof(Conditions)).Length]);
-
-            foreach (Conditions c in Enum.GetValues(typeof(Conditions)))
-            {
-                if (nRegions > 1)
-                    ConditionIDs[(int)c] = (nRegions + 1) * (int)c;
-                else
-                    ConditionIDs[(int)c] = (int)c;
-            }
-        }
-    }
+    
 
     public abstract class GonoModel : ModelInstruction
     {
@@ -102,12 +25,6 @@ namespace APACElib
         protected enum Ms { M1, M2 };  // 1st line treatment with M, and 2nd line treatment with M
         protected enum SymStates { Sym, Asym };
         protected enum ResistStates { G_0, G_A, G_B, G_AB }
-        protected enum Interventions { A1 = 2, B1, M1, B2_A, M2_A, M2_B_AB } // A1:    A is used for 1st line treatment
-                                                                             // B1:    B is used for 1st line treatment
-                                                                             // M1:    M is used for 1st line treatment
-                                                                             // B2_A:  retreating those infected with G_A with B after 1st line treatment failure
-                                                                             // M2_A:  retreating those infected with G_A with M after 1st line treatment failure
-                                                                             // M2_B_AB: retreating those infected with G_B or G_AB with M after 1st line treatment failure
         protected enum DummyParam { D_0 = 0, D_1, D_Minus1, D_Inf, T_Prev, T_DeltaPrev, N_IsolateTested, NationalLocal } // 0, 1, 2, 3, 4, 5
 
         
@@ -1251,7 +1168,6 @@ namespace APACElib
                 }
             }
 
-            // TODO: these need to be changes so that if A ever used is defined on multiple interventions 
             // if A1 ever switched off 
             _featureInfo.IfAEverOff = id;
             _epiHist.Features.Add(new Feature_Intervention(
@@ -1267,7 +1183,7 @@ namespace APACElib
                         name: "If A1 ever switched off | " + regions[regionID],
                         featureID: id++,
                         featureType: Feature_Intervention.EnumFeatureType.IfEverSwitchedOff,
-                        intervention: _decisionMaker.Interventions[(int)Interventions.A1 + regionID]) //TODO: update intervention 
+                        intervention: _decisionMaker.Interventions[(int)Interventions.A1 + regionID])
                         );
                 }
 
@@ -1286,7 +1202,7 @@ namespace APACElib
                         name: "If B1 ever switched off | " + regions[regionID],
                         featureID: id++,
                         featureType: Feature_Intervention.EnumFeatureType.IfEverSwitchedOff,
-                        intervention: _decisionMaker.Interventions[(int)Interventions.B1 + regionID + 1]) //TODO: update intervention 
+                        intervention: _decisionMaker.Interventions[(int)Interventions.B1 + regionID]) 
                         );
                 }
 
@@ -1305,7 +1221,7 @@ namespace APACElib
                         name: "If M ever switched off | " + regions[regionID],
                         featureID: id++,
                         featureType: Feature_Intervention.EnumFeatureType.IfEverSwitchedOff,
-                        intervention: _decisionMaker.Interventions[(int)Interventions.M1 + regionID + 1]) //TODO: update intervention 
+                        intervention: _decisionMaker.Interventions[(int)Interventions.M1 + regionID])
                         );
                 }
         }
