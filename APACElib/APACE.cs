9f37966342739f9309dd2387108775970182f456
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-05-04 12:58:36 -0400

    I policy

--- APACElib/APACE.cs
+++ APACElib/APACE.cs
@@ -171,7 +171,7 @@ namespace APACElib
                 _modelSettings.SimRNDSeedsSource = EnumSimRNDSeedsSource.RandomUnweighted;
 
             COVIDOptimizer optimizer =
-                new COVIDOptimizer(_excelInterface, _modelSettings, _listModelInstr, COVIDOptimizer.EnumPolicyType.RtSingleWTP);
+                new COVIDOptimizer(_excelInterface, _modelSettings, _listModelInstr, COVIDOptimizer.EnumPolicyType.ISingleWTP);
 
             optimizer.Minimize(_modelSettings.OptmzSets.XDigits);
 
